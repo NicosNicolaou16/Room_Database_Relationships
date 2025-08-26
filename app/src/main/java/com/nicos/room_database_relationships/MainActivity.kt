8c018f02c13e30c1255f829a616f66090aa0c19f
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-08-26 22:47:05 +0300

    save process

--- app/src/main/java/com/nicos/room_database_relationships/MainActivity.kt
+++ app/src/main/java/com/nicos/room_database_relationships/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -35,7 +38,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier, mainViewModel: MainViewModel = hiltViewModel()) {
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier,
+    mainViewModel: MainViewModel = hiltViewModel()
+) {
     Text(
         text = "Hello $name!",
         modifier = modifier
