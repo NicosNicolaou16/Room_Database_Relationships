4330e90a87382544536ddeed1b2db0790f20e6b3
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-08-28 22:40:52 +0300

    update the queries

--- app/src/main/java/com/nicos/room_database_relationships/data/init_database/entities/rockets/dao/PayloadWeightManyToManyDao.kt
+++ app/src/main/java/com/nicos/room_database_relationships/data/init_database/entities/rockets/dao/PayloadWeightManyToManyDao.kt
@@ -3,7 +3,11 @@ package com.nicos.room_database_relationships.data.init_database.entities.rocket
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import com.nicos.room_database_relationships.data.init_database.entities.rockets.PayloadWeightWithRocketManyToMany
 import com.nicos.room_database_relationships.data.init_database.entities.rockets.PayloadWeightsManyToManyEntity
+import com.nicos.room_database_relationships.data.init_database.entities.rockets.RocketWithPayloadWeightManyToMany
 
 @Dao
 interface PayloadWeightManyToManyDao {
@@ -13,4 +17,13 @@ interface PayloadWeightManyToManyDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(payloadWeights: MutableList<PayloadWeightsManyToManyEntity>)
+
+    @Transaction
+    @Query("SELECT * FROM RocketsEntity WHERE id = :rocketId")
+    suspend fun getRocketWithPayloads(rocketId: Int): List<RocketWithPayloadWeightManyToMany>
+
+    @Transaction
+    @Query("SELECT * FROM PayloadWeightsEntity WHERE id = :payloadId")
+    suspend fun getPayloadWithRockets(payloadId: Int): List<PayloadWeightWithRocketManyToMany>
+
 }
