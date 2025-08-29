fe7290d79c2c92bb94fb4a0558f0c81fd41b4590
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-08-29 22:43:29 +0300

    updates

--- app/src/main/java/com/nicos/room_database_relationships/MainActivity.kt
+++ app/src/main/java/com/nicos/room_database_relationships/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -25,7 +27,6 @@ class MainActivity : ComponentActivity() {
             Room_Database_RelationshipsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -36,20 +37,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(
-    name: String,
     modifier: Modifier = Modifier,
     mainViewModel: MainViewModel = hiltViewModel()
 ) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Text(
+            text = "Welcome to Room Database Relationships Example, Check the LogCat (View Model)",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Room_Database_RelationshipsTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
