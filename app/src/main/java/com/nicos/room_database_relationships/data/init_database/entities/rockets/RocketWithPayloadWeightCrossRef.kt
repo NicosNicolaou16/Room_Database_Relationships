587056a478330eb9f44a1c27dac0d681c971049d
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-09-12 21:08:53 +0300

    update the KSP and fix some warning in dao

--- app/src/main/java/com/nicos/room_database_relationships/data/init_database/entities/rockets/RocketWithPayloadWeightCrossRef.kt
+++ app/src/main/java/com/nicos/room_database_relationships/data/init_database/entities/rockets/RocketWithPayloadWeightCrossRef.kt
@@ -1,8 +1,12 @@
 package com.nicos.room_database_relationships.data.init_database.entities.rockets
 
 import androidx.room.Entity
+import androidx.room.Index
 
-@Entity(primaryKeys = ["rocketId", "payloadWeightId"])
+@Entity(
+    primaryKeys = ["rocketId", "payloadWeightId"],
+    indices = [Index(value = ["payloadWeightId"])]
+)
 data class RocketWithPayloadWeightCrossRef(
     val rocketId: Int,
     val payloadWeightId: Long,
