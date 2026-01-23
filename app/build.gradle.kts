050d3c61219298849b719a0795add9bef9e76519
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-01-23 23:12:15 +0200

    migrate Kotlin version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,15 +44,17 @@ android {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
-    kotlin {
-        compilerOptions {
-            jvmTarget = JvmTarget.fromTarget("21")
-            freeCompilerArgs = listOf("-Xannotation-default-target=param-property")
-        }
-    }
     buildFeatures {
         compose = true
         buildConfig = true
+    }
+}
+
+kotlin {
+    compilerOptions {
+        languageVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_3
+        jvmTarget = JvmTarget.fromTarget("21")
+        freeCompilerArgs = listOf("-Xannotation-default-target=param-property")
     }
 }
 
